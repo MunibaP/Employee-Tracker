f7bef7252b134a9237e32564037a3dee6bd78259
MunibaP <munibapervez596@gmail.com>
2024-03-28 16:14:59 -0400

    Fixed issues

--- db/querry.sql
+++ db/querry.sql
@@ -1,5 +1,5 @@
--- SELECT depaartment.department_name AS department, reviews.review
+-- SELECT departments.department_name AS department, reviews.review
 -- FROM 
--- LEFT JOIN department
--- ON reviews.department_id = department.id
+-- LEFT JOIN departments
+-- ON reviews.department_id = departments.id
 -- ORDER BY departments.department_name;
